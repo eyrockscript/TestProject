93f0bd4565243c271548bb899f3d04f187ba2005
eyrockscript <eyrockscript@github.com>
2017-11-28 14:37:46 -0600

    Inicio

---
+++ src/main/java/com/metalsa/testproject/TestController.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.metalsa.testproject;
+
+import javax.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ *
+ * @author eliud.trejo
+ */
+@Controller
+@RequestMapping("/")
+public class TestController {
+    
+    @RequestMapping( value = "index",method = RequestMethod.GET)
+    public String container( HttpServletRequest request, Model model) {
+        System.out.println("aaaaaaa");
+        return "index_";
+    }
+}
